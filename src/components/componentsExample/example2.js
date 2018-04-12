ab65a25b45f0a4b6e08d504fb90e6cca970788bd
David Schmetterling <schmett29@gmail.com>
2018-04-11 23:41:00 -0400

    simple numbers demo

--- src/components/componentsExample/example2.js
+++ src/components/componentsExample/example2.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 export default class ParentComponent extends Component {
 
   state = {
+    tempNumber: null,
+    currentNumber: null,
     photo_id : []
   }
 
   addNumberToArray = () => {
-    console.log(this.state.currentNumber)
+    this.setState({ currentNumber: this.state.tempNumber });
+  }
+
+  onInputChange(number) {
+    this.setState({ tempNumber: number });
+  }
+
+  clearNumber = ()  => {
+    this.setState({ currentNumber: null });
   }
 
   render() {
@@ -15,10 +25,30 @@ export default class ParentComponent extends Component {
       <div>
       {/* everything needs to be in one div */}
         <h1>Lets learn about state, folks</h1>
-          <p>Enter a number:</p> <input type="text" name="numbers" value={this.state.currentNumber}/>
-          <input type="button" name="number_button" value="submit" onClick={() => this.addNumberToArray()}/>
-          <br/>
-          <input type="button" name="clear_button" value= "clear array"/>
+          <p>Enter a number:</p>
+          <input
+            type="text"
+            name="numbers"
+            onChange={event => this.onInputChange(event.target.value)}
+          />
+          <input
+            type="button"
+            name="number_button"
+            value="submit"
+            onClick={this.addNumberToArray}
+          />
+          <input
+            type="button"
+            name="clear_button"
+            value= "clear array"
+            onClick={this.clearNumber}
+          />
+
+          {this.state.currentNumber &&
+          <div>
+            Your number is:
+            {this.state.currentNumber}
+          </div>}
       </div>
     );
   }
