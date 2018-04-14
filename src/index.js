a15610d99e4db3e97db6502350d00f48e5da0f76
David Schmetterling <schmett29@gmail.com>
2018-04-13 23:37:21 -0400

    parent component works

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import App from './components/app'
-import ParentComponent from './components/componentsExample/example2'
+import ParentComponent from './components/componentsExample/parentComponent'
 
 //EXAMPLE 1:HELLO WORLD
 
